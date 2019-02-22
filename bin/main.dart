ebac9c6a2860a33d418f778631bbedc223d586a5
zanuka <git@zanuka.com>
2019-02-23 02:19:41 +1300

    deal 'em

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,6 @@
 import 'package:holdem/cards.dart' as cards;
 
-main(List<String> arguments) {
+main() {
   var deck = cards.Deck();
 
   // deck.removeCard('Diamonds', 'Ace');
@@ -22,4 +22,14 @@ main(List<String> arguments) {
   print('Hearts = $hearts');
   print('Clubs = $clubs');
   print('Spades = $spades');
+
+  var dealOne = deck.deal(1);
+  var dealTwo = deck.deal(2);
+  var dealFive = deck.deal(5);
+  var dealSeven = deck.deal(7);
+
+  print('dealOne = $dealOne');
+  print('dealTwo = $dealTwo');
+  print('dealFive = $dealFive');
+  print('dealSeven = $dealSeven');
 }
